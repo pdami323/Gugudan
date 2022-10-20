0ae2eb2818d432d134c0227c472d24e9102dcbda
Dami Park <ekal0323@gmail.com>
2022-10-20 17:55:43 +0900

    구구단 7단 - for문

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -66,7 +66,13 @@ public class Gugudan {
 		int i = 1;
 		while(i<10) {
 			System.out.println(6*i);
-			i = i +1;
+			i = i +1;	//i++
+		}
+		
+		// 7´Ü
+		System.out.println("7´Ü");
+		for(int j=1;j<10;j++) {
+			System.out.println(7*j);
 		}
 	}
 }
